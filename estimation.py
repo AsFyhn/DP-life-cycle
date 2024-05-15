be0847a3d9e92cd29c102c2401a76576682c0da5
AsFyhn <prn917@alumni.ku.dk>
2024-05-15 15:08:19 +0200

    jj

--- estimation.py
+++ estimation.py
@@ -21,27 +21,31 @@ class SMD:
         # 3. simulate data from the model and calculate moments [have this as a complete function, used for standard errors]
         sim = self.simulator.sim_setup(self.model)
         self.simulator.draw_random(self.model, sim)
-        sim = self.simulator.simulate(sim, self.model, sol)
-        self.mom_sim = self.mom_fun(sim)
+        self.sim = self.simulator.simulate(sim, self.model, sol)
+        self.mom_sim = np.empty(4) + np.nan
+        for j, i in enumerate(range(0,len(self.sim.C_avg),10)):
+            self.mom_sim[j] = self.sim.C_avg[i:i+10].mean()
+        # self.mom_sim = self.mom_fun(self.sim)
 
         # self.model.simulate() # consider adding the solve method to the class beforehand
         # self.mom_sim = self.mom_fun(self.model.sim)
 
         # 4. calculate objective function and return it
-        self.diff = self.mom_data.C_avg - self.mom_sim
+        self.diff = self.mom_data - self.mom_sim
         self.obj  = (np.transpose(self.diff) @ W) @ self.diff
 
         return self.obj 
 
     def estimate(self,theta0,est_par,W):
-        assert(len(W[0])==len(self.mom_data.C_avg)) # check dimensions of W and mom_data
+        assert(len(W[0])==len(self.mom_data)) # check dimensions of W and mom_data
 
         self.est_out = minimize(self.obj_function, theta0, (est_par,W,), method='nelder-mead',options={'disp': False})        
         # return output
         self.est = self.est_out.x
         self.W = W
+
     def estimate_using_grid(self,beta_grid,rho_grid,est_par,W,):
-        assert(len(W[0])==len(self.mom_data.C_avg)) # check dimensions of W and mom_data
+        assert(len(W[0])==len(self.mom_data)) # check dimensions of W and mom_data
 
         self.beta_grid = beta_grid
         self.rho_grid = rho_grid
