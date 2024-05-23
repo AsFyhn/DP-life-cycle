f73f2e5be4b2513f982738d04013ceb9420eaa28
AsFyhn <prn917@alumni.ku.dk>
2024-05-23 13:46:12 +0200

    colors of the lines

--- functions/plotgenerator.py
+++ functions/plotgenerator.py
@@ -4,11 +4,17 @@ import numpy as np
 from scipy import stats
 
 class PlotFigure:
-    def __init__(self, figsize=(8, 6),fontname:str='Century Gothic'):
+    def __init__(self, figsize=(8, 6),fontname:str='Century Gothic',color_cycle=None):
         self.fig, self.ax = plt.subplots(figsize=figsize)
         self.plots = []
         self.fontname = fontname
         self.ax.tick_params(axis='y')
+        color_cycle 
+        # plt.style.use('seaborn-white')
+        if color_cycle is None:
+            color_cycle = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf', '#aec7e8', '#ffbb78', '#98df8a', '#ff9896', '#c5b0d5', '#c49c94', '#f7b6d2', '#c7c7c7', '#dbdb8d', '#9edae5']
+            # color_cycle = plt.rcParams['axes.color_cycle'][2] #plt.get_cmap('Set1').colors # sns.color_palette("pastel")
+        self.ax.set_prop_cycle('color', color_cycle)
     def add_histogram(self, data, bins=10, label='',normal_distribution=False, color=None):
         """
         Add a histogram to the figure.
@@ -42,6 +48,7 @@ class PlotFigure:
         if axis == 'y2':
             if not hasattr(self, 'ax_sec'):
                 self.add_secondary_yaxis()
+                self.ax_sec.set_prop_cycle('color', self.ax._get_lines.prop_cycler)
                 obj = self.ax_sec
         else: 
             obj = self.ax
